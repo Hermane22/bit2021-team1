a3df26730fa7d8bd3f1e83eed7cfe1cd5d879261
Raoul <sawadogoraoule@gmail.com>
2021-10-11 12:12:26 +0000

    I wrote the backend code that list the contacts on the first page

---
+++ index.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+$bdd=new PDO('mysql:host=localhost;dbname=dps','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>Contact Form</title>
+</head>
+<body>
+
+	<div class="container">
+		<!--  SEARCH FORM -->
+		<header class="header">
+			
+			<form class="search-bar">
+				<input type="search-name" class="contact-search" name="search-area" placeholder="Search">
+			</form>
+
+			<!--  ADD-CONTACT BUTTON/ICON -->
+			<a href="edit.php"><i>+</i></a>
+		</header>
+
+		<!--  CONTACT LIST -->
+		<section class="contacts-library">
+			<ul class="contacts-list">
+				
+			<?php
+             $reponse=$bdd->query('SELECT * FROM list  ORDER BY datet ASC');
+        while($donnees=$reponse->fetch()){ ?>
+				<a href="contact-profile.php">
+					<div class="contact-section">
+						<li class="list__item">
+							<p class="contact-name"><?php echo $donnees['fullname'] ; ?></p>
+							<p class="relationship"><?php echo $donnees['relationship'] ; ?></p>
+						</li>
+
+					</div>
+				</a>
+
+				<hr>
+				<?php
+					}
+				?>
+				
+
+
+				
+			</ul>
+		</section>
+	</div>
+
+</body>
+</html>
+ 
