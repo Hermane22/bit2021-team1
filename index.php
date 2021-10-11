4b4e60248def5972734a5b55d230a4eb57666fe1
Hermane22 <hermanebagoro@gmail.com>
2021-10-11 10:24:15 +0000

    page layout for contact list display

--- index.php
+++ index.php
@@ -4,10 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" type="text/css" href="style.css">
     <title>contact App</title>
 </head>
 <body>
+	<div class="container">
+		<!--  SEARCH FORM -->
+		<header class="header">
+			
+			<form class="search-bar">
+				<input type="search-name" class="contact-search" name="search-area" placeholder="Search contacts">
+			</form>
 
+			<!--  ADD-CONTACT BUTTON/ICON -->
+			<a href="edit.php">+</i></a>
+		</header>
+
+		<!--  CONTACT LIST -->
+		<section class="contacts-library">
+			<ul class="contacts-list">
+
+				<a href="contact-profile.php">
+					<div class="contact-section">
+						<li class="list__item">
+							<p class="contact-name">Hermane BAGORO</p>
+							<p class="relationship">Friend</p>
+						</li>
+
+					</div>
+				</a>
+
+
+			</ul>
+		</section>
+	</div>
 
 </body>
 </html>
