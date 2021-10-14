2ea7a6a4b8787589ebf8cb39138116377e03dc04
Hermane22 <hermanebagoro@gmail.com>
2021-10-14 16:50:34 +0000

    creation of contact profile page

---
+++ contact-profile.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles-profile.css">
+
+	<title>Contact Profile</title>
+</head>
+<body>
+
+	<div class="container">
+		<header class="hero">
+			<a href="index.php">
+				<img src="icone/arrow-left-circle.svg" alt="arrow">
+			</a>
+			<div class="hero-info">
+				<h1 class="name">Hermane BAGORO</h1>
+				<img src="icone/user.svg" alt="user" style = "width: 80px; color: white ">
+				<p  class="relationship">Friend</p>
+			</div>
+		</header>
+
+		<section class="contact-info">
+
+			<div class="info-line">
+				<img src="icone/phone-call.svg" alt="phone" >
+				<p class="phone-number">888.888.888</p>
+			</div>
+
+		
+
+			<div class="info-line">
+				<img src="icone/mail.svg" alt="mail">
+				<p class="email">hermanebagoro@gmail.com</p>
+			</div>
+
+			
+
+			<div class="info-line">
+				<img src="icone/map-pin.svg" alt="map">
+				<p class="address">Palogo</p>
+			</div>
+		</section>
+
+		<a href="edit.php">
+			<section class="edit-contact">
+				<div class="edit-button">
+					<img src="icone/settings.svg" alt="setting">
+				</div>
+			</section>
+		</a>
+	</div>
+
+</body>
+</html>
