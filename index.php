05d0b99970d844a63d99e39891b3fcffb5c0566e
Hermane22 <hermanebagoro@gmail.com>
2021-10-14 16:47:36 +0000

    correction of some parts

--- index.php
+++ index.php
@@ -22,32 +22,43 @@ $bdd=new PDO('mysql:host=localhost;dbname=dps','root','akatsuki',array(PDO::ATTR
 		<header class="header" style= background-color: white; >
 			
 			<form class="search-bar">
-				<input type="search-name" class="contact-search" name="search-area" placeholder="  search contact">
+				<input  type="search-name" class="contact-search" name="search-area" placeholder="  search contact" >
 			</form>
+				<!--  ADD-CONTACT BUTTON/ICON -->
+	<div class ="button-add">
+		<a href="edit.php"><i> + </i></a>
+	</div>
 
 		</header>
-		<hr>
+
 
 		<!--  CONTACT LIST -->
 		<section class="contacts-library">
 			<ul class="contacts-list">
 
+			<?php
+             $reponse=$bdd->query('SELECT * FROM list  ORDER BY datet ASC');
+        while($donnees=$reponse->fetch()){ ?>
+
 				<a href="contact-profile.php">
 					<div class="contact-section">
 						<li class="list__item">
-							<p class="contact-name">Hermane BAGORO</p>
-							<p class="relationship">Friend</p>
+							<p class="contact-name"><?php echo $donnees['fullname'] ; ?></p>
+							<p class="relationship"><?php echo $donnees['relationship'] ; ?></p>
 						</li>
-
 					</div>
-				</a>
 
+
+				</a>
 			</ul>
+			<?php
+					}
+				?>
 		</section>
 	</div>
 
-	<!--  ADD-CONTACT BUTTON/ICON -->
-			<a href="edit.php"><i> + </i></a>
+
+			
 
 </body>
 </html>
