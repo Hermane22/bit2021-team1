6e06b4bf548c0753b8bb55e6c73ea75ad0047d40
Moctar26 <guiramoctar26@gmail.com>
2021-10-14 19:26:03 +0200

    adding the backend of dynamic list showing of index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$bdd=new PDO('mysql:host=localhost;dbname=dps','root','akatsuki',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd=new PDO('mysql:host=localhost;dbname=dps','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 ?>
 
@@ -22,28 +22,38 @@ $bdd=new PDO('mysql:host=localhost;dbname=dps','root','akatsuki',array(PDO::ATTR
 		<header class="header" style= background-color: white; >
 			
 			<form class="search-bar">
-				<input type="search-name" class="contact-search" name="search-area" placeholder="  search contact">
+				<input type="search-name" class="contact-search" name="search-area" placeholder="search contact">
 			</form>
 
 		</header>
 		<hr>
+        
 
 		<!--  CONTACT LIST -->
 		<section class="contacts-library">
-			<ul class="contacts-list">
+				<ul class="contacts-list">
+			
+					<?php
+						$reponse=$bdd->query('SELECT * FROM list  ORDER BY fullname');
+						while($donnees=$reponse->fetch()){ ?>
 
-				<a href="contact-profile.php">
-					<div class="contact-section">
-						<li class="list__item">
-							<p class="contact-name">Hermane BAGORO</p>
-							<p class="relationship">Friend</p>
-						</li>
+					<a href="contact-profile.php?id=<?php echo $donnees['N']; ?>">
+		
+						<div class="contact-section">
+							<li class="list__item">
+								<p class="contact-name"><?php echo $donnees['fullname'] ; ?></p>
+								<p class="relationship"><?php echo $donnees['relationship'] ; ?></p>
+							</li>
+						</div>
+					</a>
 
-					</div>
-				</a>
-
-			</ul>
-		</section>
+					<hr>
+					<?php
+						}
+					?>
+					
+				</ul>
+			</section>
 	</div>
 
 	<!--  ADD-CONTACT BUTTON/ICON -->
