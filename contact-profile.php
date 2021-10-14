c89e25afdb28b90fcd453cfe056bf63e69685420
Moctar26 <guiramoctar26@gmail.com>
2021-10-14 21:34:23 +0200

    backend for showing the concerned contact details

---
+++ contact-profile.php
@@ -0,0 +1,52 @@
+<?php
+	session_start();
+	$bdd=new PDO('mysql:host=localhost;dbname=dps','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$reponse=$bdd->query("SELECT * FROM list WHERE N=". $_GET['id']);
+	$donnees=$reponse->fetch();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="styles-profile.css">
+	<title>Contact Profile</title>
+</head>
+<body>
+	<div class="container">
+		<header class="hero">
+			<a href="index.php">
+				<img src="icone/arrow-left-circle.svg" alt="arrow">
+			</a>
+			<div class="hero-info">
+				<h1 class="name"><?php echo $donnees['fullname']; ?></h1>
+				<img src="icone/user.svg" alt="user" style = "width: 80px; color: white ">
+				<p  class="relationship"><?php echo $donnees['relationship']; ?></p>
+			</div>
+		</header>
+		<section class="contact-info">
+			<div class="info-line">
+				<img src="icone/phone-call.svg" alt="phone" >
+				<p class="phone-number"><?php echo $donnees['phone']; ?></p>
+			</div>
+		
+			<div class="info-line">
+				<img src="icone/mail.svg" alt="mail">
+				<p class="email"><?php echo $donnees['mail']; ?></p>
+			</div>
+			
+			<div class="info-line">
+				<img src="icone/map-pin.svg" alt="map">
+				<p class="address"><?php echo $donnees['addresse']; ?></p>
+			</div>
+		</section>
+		<a href="edit.php?id=<?php echo $donnees['N'];?>">
+			<section class="edit-contact">
+				<div class="edit-button">
+					<img src="icone/settings.svg" alt="setting">
+				</div>
+			</section>
+		</a>
+	</div>
+</body>
+</html>
