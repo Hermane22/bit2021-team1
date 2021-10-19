b404eeabeaf16bff14d986abd2b8293cb5a94f15
Hermane22 <hermanebagoro@gmail.com>
2021-10-19 22:57:37 +0000

    retouching

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php
 if(isset($_GET['id'])){
 	session_start();
-	$bdd=new PDO('mysql:host=localhost;dbname=dps','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	$bdd=new PDO('mysql:host=localhost;dbname=dps','root','akatsuki',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 	$reponse=$bdd->query("SELECT * FROM list WHERE N=". $_GET['id']);
 	$donnees=$reponse->fetch();
 }
@@ -14,7 +14,6 @@ if(isset($_GET['id'])){
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style-ed.css">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	
 	<title>Edit Contact</title>
 </head>
@@ -24,52 +23,52 @@ if(isset($_GET['id'])){
 		<header class="hero">
 			
 			<a href="index.php">
-			<i class="fas fa-arrow-left"></i>
+			<img src="icone/arrow-left-circle.svg" alt="arrow">
 			</a>
 			<div class="hero-info">
+				<img src="icone/user.svg" alt="phone">
                 <h1 class="name"><?php if(isset($_GET['id'])){echo $donnees['fullname'];}else{ echo "<h3>Add New Contact</h3>";} ?></h1>
-				<p class="relationship-hero"><?php if(isset($_GET['id'])){echo $donnees['relationship'];} ?></p>
-               <img src="IMG/user1.png" alt="utili">
+
 			</div>
 		</header>
 
 		<form action="<?= isset($_GET['id'])?'update.php?id='.$_GET['id']:'request.php' ?>" method="POST">
-		<section class="contact-info">
+	<section class="contact-info">
 
-		<div class="info-line">
-			   <i class="fas fa-user"></i>
+	<div class="info-line">
+				<img src="icone/user.svg" alt="phone">
 				<input required type="text" class="type" name="fullname" placeholder="Full Name" value="<?php if(isset($_GET['id'])){ echo $donnees['fullname']; }?>">
+				
 					
 		</div>
 
 			<div class="info-line">
-		      	<i class="fas fa-heart"></i>
+				<img src="icone/heart.svg" alt="phone">
 				<input required type="text" class="type" name="relationship" placeholder="Relationship" value="<?php if(isset($_GET['id'])){echo $donnees['relationship'];} ?>">
 			</div>
 
 			<div class="info-line">
-			    <i class="fas fa-phone"></i> 
+				<img src="icone/phone-call.svg" alt="phone">
 				<input required type="text" class="type" name="phone" placeholder="Phone Number" value="<?php if(isset($_GET['id'])){echo $donnees['phone'];} ?>"> 
 			</div>
 
 			<div  class="info-line">
-		     	<i class="far fa-envelope"></i>
+				<img src="icone/mail.svg" alt="phone">
 				<input required type="text" class="type" name="mail" placeholder="Email" value="<?php if(isset($_GET['id'])){ echo $donnees['mail']; }?>">
 			</div>
 
 			
 
 			<div class="info-line">
-			    <i class="fas fa-address-card"></i>
-					<input required type="text" class="type" name="addresse" placeholder="Address" value="<?php if(isset($_GET['id'])) {echo $donnees['addresse'];} ?>">
+				<img src="icone/map-pin.svg" alt="phone">
+				<input required type="text" class="type" name="addresse" placeholder="Address" value="<?php if(isset($_GET['id'])) {echo $donnees['addresse'];} ?>">
 			</div>
 			
 
-		</section>
+	</section>
 
 		<section class="button-container">
 			<div class="update-contact">
-			    <i class="far fa-save"></i>
 				<input type="submit" value="Save" name="update">
 			</div>
 		</section>
