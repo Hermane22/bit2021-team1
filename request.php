f3f3853d690c5e2168eeeb9941a5ea2f7c1cb89a
Hermane22 <hermanebagoro@gmail.com>
2021-10-19 23:04:58 +0000

    I forgot to remove the root password so for the page to work well for you please remove it first

--- request.php
+++ request.php
@@ -1,5 +1,5 @@
 <?php
-$bdd=new PDO('mysql:host=localhost;dbname=dps','root','',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd=new PDO('mysql:host=localhost;dbname=dps','root','akatsuki',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 $requete=$bdd->prepare('INSERT INTO list(fullname,relationship,phone,mail,addresse,datet) VALUES (?,?,?,?,?,Now())');
 
 $requete->execute(array($_POST['fullname'],$_POST['relationship'], $_POST['phone'],$_POST['mail'],$_POST['addresse']));
